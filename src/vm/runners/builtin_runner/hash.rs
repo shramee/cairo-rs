d541e639bb320aa6239cfb67c7422b74cb9c4408
Mario J. Rugiero <mrugiero@gmail.com>
2022-11-05 16:56:58 -0300

    [POC] Migrate Pedersen computation to eqlabs' implementation

--- src/vm/runners/builtin_runner/hash.rs
+++ src/vm/runners/builtin_runner/hash.rs
@@ -1,7 +1,8 @@
 use num_bigint::{BigInt, Sign};
 use num_integer::Integer;
 use num_traits::ToPrimitive;
-use starknet_crypto::{pedersen_hash, FieldElement};
+
+use stark_hash::{stark_hash, StarkHash};
 
 use crate::bigint;
 use crate::math_utils::safe_div;
@@ -94,19 +95,19 @@ impl HashBuiltinRunner {
             self.verified_addresses.push(address.clone());
 
             //Convert MaybeRelocatable to FieldElement
-            let a_string = num_a.to_str_radix(10);
-            let b_string = num_b.to_str_radix(10);
+            let a_string = num_a.to_str_radix(16);
+            let b_string = num_b.to_str_radix(16);
             let (y, x) = match (
-                FieldElement::from_dec_str(&a_string),
-                FieldElement::from_dec_str(&b_string),
+                StarkHash::from_hex_str(&a_string),
+                StarkHash::from_hex_str(&b_string),
             ) {
                 (Ok(field_element_a), Ok(field_element_b)) => (field_element_a, field_element_b),
                 _ => return Err(RunnerError::FailedStringConversion),
             };
             //Compute pedersen Hash
-            let fe_result = pedersen_hash(&x, &y);
+            let fe_result = stark_hash(x, y);
             //Convert result from FieldElement to MaybeRelocatable
-            let r_byte_slice = fe_result.to_bytes_be();
+            let r_byte_slice = fe_result.to_be_bytes();
             let result = BigInt::from_bytes_be(Sign::Plus, &r_byte_slice);
             return Ok(Some(MaybeRelocatable::from(result)));
         }
